9a2be6196cf3e2163e70ae71e823c47d6851029e
WojciechGalecki <wojciech.galecki@wp.pl>
2019-11-09 14:40:31 +0100

    added configuration for postgresql

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,7 +3,8 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
-from flaskblog.config import Config
+from flaskblog.config import DevConfig
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -13,11 +14,12 @@ login_manager.login_message_category = 'info'
 mail = Mail()
 
 
-def create_app(config_class=Config):
+def create_app(config_class=DevConfig):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
     db.init_app(app)
+    migrate = Migrate(app, db)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,9 +3,24 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('FLASKBLOG_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    DB_USER = os.environ.get('FLASKBLOG_DATABASE_USER')
+    DB_PASSWORD = os.environ.get('FLASKBLOG_DATABASE_PASSWORD')
+    DB_HOST = os.environ.get('FLASKBLOG_DATABASE_HOST')
+    DB_DATABASE = os.environ.get('FLASKBLOG_DATABASE')
+    DB_PORT = 5432
+    SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('FLASKBLOG_EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('FLASKBLOG_EMAIL_PASS')
+
+
+class DevConfig(Config):
+    DEBUG = True
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+
+
+class ProdConfig(Config):
+    DEBUG = False
