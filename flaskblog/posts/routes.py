37900d493baf5d59cc234492bd5597d889f2bf94
WojciechGalecki <wojciech.galecki@wp.pl>
2019-11-09 20:12:06 +0100

    added logging in posts routes

--- flaskblog/posts/routes.py
+++ flaskblog/posts/routes.py
@@ -4,6 +4,7 @@ from flask_login import login_required, current_user
 from flaskblog import db
 from flaskblog.models import Post
 from flaskblog.posts.forms import PostForm
+from flaskblog.logger import logger
 
 posts = Blueprint('posts', __name__)
 
@@ -15,6 +16,7 @@ def new_post():
     if form.validate_on_submit():
         save_valid_post(form)
         flash('Successfully created new post!', 'success')
+        logger.info(f'Added new post {form.title} by user: {current_user}')
         return redirect(url_for('main.home'))
     return render_template('create_post.html', title='New Post', form=form, legend='New Post')
 
@@ -43,6 +45,7 @@ def update_post(post_id):
         post.content = form.content.data
         db.session.commit()
         flash('Successfully updated a post!', 'success')
+        logger.info(f'Updated post {post.title} by user: {post.author}')
         return redirect(url_for('posts.post', post_id=post.id))
     elif request.method == 'GET':
         form.title.data = post.title
@@ -59,4 +62,5 @@ def delete_post(post_id):
     db.session.delete(post)
     db.session.commit()
     flash('Successfully deleted a post!', 'success')
+    logger.info(f'Deleted post {post.title} by user: {post.author}')
     return redirect(url_for('main.home'))
